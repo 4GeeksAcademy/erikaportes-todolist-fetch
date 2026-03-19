5adac6473e92cb7b517de19b1b52fd892bc38011
erikaportes <152933693+erikaportes@users.noreply.github.com>
2026-03-19 01:19:53 +0000

    changes

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,17 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-//create your first component
+const URL = "https://playground.4geeks.com/todo/users/erikaportes";
+const USER = "erikaportes";
+
 const ToDoList = () => {
 
 	const [inputTarea, setInputTarea] = useState("")
 	const [listaTareas, setListaTareas] = useState([])
 
-	function crearTarea(Enter) {
+	console.log(listaTareas)
+	async function crearTarea(Enter) {
 		if (Enter === "Enter" && inputTarea.trim() !== "") {
-			setListaTareas([...listaTareas, inputTarea])
-			setInputTarea("")
+
+			const response = await fetch('https://playground.4geeks.com/todo/todos/erikaportes', {
+				method: 'POST',
+				body: JSON.stringify({
+					"label": inputTarea,
+					"is_done": false
+				}),  // la variable dataToSend puede ser un 'string' o un {objeto} que proviene de algún lugar más arriba en nuestra aplicación
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+			if (response.ok) {
+				const data = await response.json();
+				setInputTarea("")
+				traerTarea()
+				return data;
+			} else {
+				console.log('error: ', response.status, response.statusText);
+				/* Realiza el tratamiento del error que devolvió el request HTTP */
+				return { error: { status: response.status, statusText: response.statusText } };
+			};
+
 		}
 	}
+
+	const traerTarea = async () => {
+		const response = await fetch('https://playground.4geeks.com/todo/users/erikaportes');
+		if (response.ok) {
+			const data = await response.json();
+			setListaTareas(data.todos)
+			return data;
+		} else {
+			console.log('error: ', response.status, response.statusText);
+			/* Realiza el tratamiento del error que devolvió el request HTTP */
+			return { error: { status: response.status, statusText: response.statusText } };
+		};
+	};
+
+	useEffect(() => { traerTarea() }, [])
 
 	function trash(indice) {
 		setListaTareas(listaTareas.filter((item, i) => indice != i))
@@ -36,7 +74,7 @@ const ToDoList = () => {
 							<li
 								className="list-group-item d-flex justify-content-between align-items-center tarea-item"
 								key={tarea + indice} >
-								{tarea}
+								{tarea.label}
 								<i
 									onClick={() => trash(indice)}
 									className="fa-solid fa-trash-can trash-icon ms-auto">
